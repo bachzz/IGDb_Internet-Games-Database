82870b03d8fbaf3fc9d1088d36c2e1aaa33a3105
giang <trgiang1998@gmail.com>
2019-06-17 19:00:15 +0700

    commit share.php for deployment

--- shared.php
+++ shared.php
@@ -2,9 +2,9 @@
 	
 	// db info
 
-	$host= 'localhost';
+	$host= 'ec2-75-101-128-10.compute-1.amazonaws.com';
   	$port= '5432';
-  	$user= 'giang';
-  	$pass= 'abcdef090';
-  	$dbname= 'igdb_db';  
+  	$user= 'bweivjoovhuujl';
+  	$pass= 'b97bdd6d011debd7432a0d52cfa0ad21015c3018eb969d45d1a00390ae111feb';
+  	$dbname= 'dcc4kraojefjci';  
 ?>
